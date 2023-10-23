b31fdb157b7c5bc08c9819404f08dc0add73d716
Ryan <466010106@qq.com>
2023-10-23 20:05:33 +0800

    add flask

--- app/src/main/java/com/example/finalwork/User_info_xjq.kt
+++
@@ -1,3 +0,0 @@
-package com.example.finalwork
-
-class User_info_xjq (val account:Int, val nickname: String, val avatar: Int) {}
--- app/src/main/java/com/example/finalwork/User_xjq.kt
+++
@@ -1,3 +0,0 @@
-package com.example.finalwork
-
-class User_xjq (val account:Int, val password:String) {}
